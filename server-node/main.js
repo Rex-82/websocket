d7122a47eb779c61f09800e78036ea36e9f42ba2
Simone Ferretti <simone.ferretti.work@gmail.com>
2024-07-18 23:38:28 +0200

    chore: comment out static serving, add todos

--- server-node/main.js
+++ server-node/main.js
@@ -1,4 +1,6 @@
 import { WebSocketServer } from "ws";
+
+/*
 import path from "node:path";
 import express from "express";
 const app = express();
@@ -6,20 +8,6 @@ const app = express();
 app.use("/", express.static(path.resolve(import.meta.dirname, "../client")));
 
 const expressServer = app.listen(9876); // regular http server using node express which serves your webpage
-
-const wsServer = new WebSocketServer({
-	port: 8080,
-}); // a websocket server
-
-wsServer.on("connection", (ws) => {
-	// what should a websocket do on connection
-	ws.on("message", (msg) => {
-		// what to do on message event
-		for (const client of wsServer.clients) {
-			client.send(msg.toString());
-		}
-	});
-});
 
 expressServer.on("upgrade", async function upgrade(request, socket, head) {
 	//handling upgrade(http to websocekt) event
@@ -33,5 +21,27 @@ expressServer.on("upgrade", async function upgrade(request, socket, head) {
 	//emit connection when request accepted
 	wsServer.handleUpgrade(request, socket, head, function done(ws) {
 		wsServer.emit("connection", ws, request);
+	});
+});
+*/
+
+// TODO: keep messages history
+
+// TODO: Send history to new connecting user
+
+// TODO: Handle custom sessions(?)
+
+const wsServer = new WebSocketServer({
+	port: 8080,
+}); // a websocket server
+
+wsServer.on("connection", (ws) => {
+	// what should a websocket do on connection
+	ws.on("message", (msg) => {
+		// what to do on message event
+		for (const client of wsServer.clients) {
+			console.log(msg);
+			client.send(msg.toString());
+		}
 	});
 });
